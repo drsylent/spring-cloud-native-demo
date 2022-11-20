1409ad719282ed0b24aafe2f0e7af57c60829bf7
Csendes Dávid <csendes.david@alerant.hu>
2022-11-20 12:41:41 +0100

    Deleting unnecessary test, integration test is in its own phase

--- src/test/java/hu/alerant/cloud/api/DemoControllerIT.java
+++ src/test/java/hu/alerant/cloud/api/DemoControllerIT.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class DemoControllerIntegrationTest {
+class DemoControllerIT {
 
     @Autowired
     private MockMvc mvc;
